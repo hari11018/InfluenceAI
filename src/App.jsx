8a4509002688cf2c3c45c22a6f90a38f0d14be2f
hari11018 <harinath11018@gmail.com>
2023-06-16 19:25:05 +0530

    Version v1

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import Header from "./Header";
+import HomePage from "./HomePage";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Header />
+          <Routes>
+            {/* <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} /> */}
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
