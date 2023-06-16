74e80d76edb3244d46ddc2b2dcdc6bd673d2c30c
hari11018 <harinath11018@gmail.com>
2023-06-16 22:10:22 +0530

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
-import Header from "./Header";
-import HomePage from "./HomePage";
+import Details from "./src/Details";
+import SearchParams from "./src/SearchParams";
+import Header from "./src/Header";
+import HomePage from "./src/HomePage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
